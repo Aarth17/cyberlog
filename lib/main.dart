5cf5d3b92ff72094c7c7304af1e67faa9b518171
Aarth17 <aarthmistry17@gmail.com>
2025-12-03 14:02:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("If Lost, Contact"),
+        ),
+        body: const Center(
+          child: Text(
+            "Name: Aarth\nPhone: +91 99999 99999",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
